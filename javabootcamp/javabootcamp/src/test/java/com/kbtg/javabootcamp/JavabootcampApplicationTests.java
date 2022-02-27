30a238d5eb03640f1d3fabe6a1402895987c7f56
ganinw13120 <gan131201@gmail.com>
2022-02-27 14:15:32 +0700

    Initialize project #1

--- javabootcamp/javabootcamp/src/test/java/com/kbtg/javabootcamp/JavabootcampApplicationTests.java
+++ javabootcamp/javabootcamp/src/test/java/com/kbtg/javabootcamp/JavabootcampApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.javakbtgbootcamp;
+package com.kbtg.javabootcamp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaKbtgBootcampApplicationTests {
+class JavabootcampApplicationTests {
 
 	@Test
 	void contextLoads() {
